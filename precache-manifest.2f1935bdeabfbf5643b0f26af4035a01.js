c2a8c56a713c3c3c213d5cfd00a6d5f6d15135ad
AVS1508 <aditya.11.present@gmail.com>
2020-06-15 12:44:14 +0530

    Updates

---
+++ precache-manifest.2f1935bdeabfbf5643b0f26af4035a01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e60509a593257e59329133cad468e0",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "e3fa91ff33db04bc3885",
+    "url": "/dashboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1cd9aa1b4a9dbe6bf102",
+    "url": "/dashboard/static/css/main.053ec40f.chunk.css"
+  },
+  {
+    "revision": "e3fa91ff33db04bc3885",
+    "url": "/dashboard/static/js/2.a827afde.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dashboard/static/js/2.a827afde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd9aa1b4a9dbe6bf102",
+    "url": "/dashboard/static/js/main.a9b69f18.chunk.js"
+  },
+  {
+    "revision": "6ce332862785f968006a",
+    "url": "/dashboard/static/js/runtime-main.8fb46908.js"
+  },
+  {
+    "revision": "7944da921d8b31556ddfe5d1afd4c051",
+    "url": "/dashboard/static/media/placeholder.7944da92.jpg"
+  },
+  {
+    "revision": "7dae01c53e4844ae35f5dda7fef96580",
+    "url": "/dashboard/static/media/professional-me.7dae01c5.jpg"
+  }
+]);
